c6d4f6005bc83b917bef4f4954d3f75cf41bd6c2
Yerden Zhumabekov <yerden.zhumabekov@gmail.com>
2019-11-22 12:18:41 +0600

    scripts: fix build
    Signed-off-by: Yerden Zhumabekov <yerden.zhumabekov@gmail.com>

--- scripts/build.dpdk.sh
+++ scripts/build.dpdk.sh
@@ -8,10 +8,7 @@ LD_CONF=/etc/ld.so.conf.d/dpdk.conf
 trap "rm -fr $BUILD $REPO $FILELIST" EXIT
 
 # args
-TARBALL=$1
-
-# where to install
-DESTDIR=/
+TARBALL=build/dpdk.tar.gz
 
 # take first ninja available
 NINJA=$(which ninja-build ninja|head -1)
@@ -30,26 +27,57 @@ $NINJA && $NINJA install) || exit 1
 #meson configure -Dtests=false && \
 
 pushd $(dirname $0)
-# collect libdir
+#
+# let's collect what we take to next stage
+# register directories with libraries to $LD_CONF
+#
 python3 ./meson-installed.py $BUILD/meson-info/intro-installed.json | \
 (while read file; do
 	case $file in
 		*.so*|*.a) dirname $file ;;
 	esac
 done) | sort | uniq > $LD_CONF || exit 1
+
+# store $LD_CONF to $FILELIST
 echo "$LD_CONF" >> $FILELIST
 
-# list all files
+# enlist all files into $FILELIST
 # *.so* is a special case because we need all symlinks too
+# also move  *.pc to /usr/local/share/pkgconfig
 python3 ./meson-installed.py $BUILD/meson-info/intro-installed.json | \
 (while read file; do
 	case $file in
-		*.so*) ls $(echo $file|awk 'BEGIN{OFS=FS="."}{while($NF!="so")NF--; print}')* ;;
-		    *) echo $file ;;
+		*.so*)
+			ls $(echo $file|awk 'BEGIN{OFS=FS="."}{while($NF!="so")NF--; print}')*
+			;;
+		*.pc)
+			mkdir -p /usr/local/share/pkgconfig
+			mv $file /usr/local/share/pkgconfig
+			ls /usr/local/share/pkgconfig/$(basename $file)
+			;;
+		*)
+			echo $file
+			;;
 	esac
 done) | sort | uniq >> $FILELIST || exit 1
 popd
 
-# archive all files
-tar cfz $TARBALL -P -T $FILELIST || exit 1
+#
+# save all files into TARBALL
+#
+case $TARBALL in
+	*.gz) TAR_OPTS="cfz" ;;
+	*.bz2) TAR_OPTS="cfj" ;;
+	*) TAR_OPTS="cf" ;;
+esac
+tar $TAR_OPTS $TARBALL -P -T $FILELIST || exit 1
+
+# change owner to DOCKER_UID
 chown $DOCKER_UID $TARBALL || exit 1
+
+cat > build/Dockerfile <<EOF
+FROM ${BASE}:${DIST}
+LABEL org.opencontainers.image.authors="yerden.zhumabekov@gmail.com"
+LABEL org.opencontainers.image.created="$(date --rfc-3339='seconds')"
+ADD $TARBALL /
+EOF
