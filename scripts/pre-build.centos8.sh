def020026b7b0ad9dc9652b1ca7c26f0a62be24c
Yerden Zhumabekov <yerden.zhumabekov@gmail.com>
2021-07-26 18:00:51 +0600

    add Rocky Linux support [WIP]
    Signed-off-by: Yerden Zhumabekov <yerden.zhumabekov@gmail.com>

---
+++ scripts/pre-build.centos8.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+LOG=`mktemp`
+trap "rm -f $LOG" EXIT
+
+PACKAGES="\
+	gcc \
+	glibc-devel \
+	kernel-headers \
+	kernel-devel \
+	numactl-devel \
+	libibverbs-devel \
+	jansson-devel \
+	zlib-devel \
+	python3-pip \
+	libpcap-devel \
+	libmnl-devel \
+	ninja-build \
+	git \
+	make \
+	pkgconfig \
+	cmake \
+"
+
+(dnf install -y epel-release dnf-plugins-core && \
+  dnf config-manager --set-enabled powertools && \
+  dnf install -y $PACKAGES > $LOG)
+if [ $? != 0 ]; then
+        cat $LOG
+        exit 1
+fi
+echo "Packages installed OK."
+
+#
+# container's uname reports host kernel,
+# we have to trick uname's users.
+#
+mv /bin/uname /bin/uname.orig
+cat > /bin/uname << EOF
+#!/bin/bash
+if [ "\$1" != "-r" ]; then /bin/uname.orig \$@
+else ls -v /usr/src/kernels |tail -1
+fi
+EOF
+chmod +x /bin/uname || exit 1
+echo "uname replaced OK."
+
+mkdir -p /lib/modules/$(uname -r)
+ln -sf /usr/src/kernels/$(uname -r) /lib/modules/$(uname -r)/build
+echo "Created symlink in /lib/modules OK."
